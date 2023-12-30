0dd4d5e42bdca3328e0b63b8b9a524e320ac2d48
Developer <izimansuper1@gmail.com>
2023-12-30 23:31:33 +0200

    filewrite test #7

--- src/actions/products/addProductAction.ts
+++ src/actions/products/addProductAction.ts
@@ -42,8 +42,8 @@ export const addProductAction = async (data: FormData): Promise<iProduct | undef
     }) as iProduct;
 
     if (thumb?.size) {
-      const savePath = path.join(process.cwd(), "src/uploads/products/" + thumbName);
-            
+      const savePath = path.join(process.cwd(), "/static/products/" + thumbName);
+
       //await thumbSaveService(thumb, thumbName, 'products');
       await thumbSaveServiceTest(thumb, savePath)
     }
