9b3fea9d5deb0a3a2e9513e4538d4244293aa98c
Developer <izimansuper1@gmail.com>
2023-12-30 15:09:17 +0200

    revalidate test

--- src/actions/products/getProductsAction.ts
+++ src/actions/products/getProductsAction.ts
@@ -1,8 +1,8 @@
-'use server'
-
 import { DB_CONNECT } from "@/db"
 import ProductsModel from '@/db/models/ProductsModel'
 import { iProduct } from "@/types"
+
+export const revalidate = 1;
 
 export const getProductsAction = async (): Promise<iProduct[] | undefined> => {
   try {
